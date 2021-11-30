f4039d99d4cef3dc2ddf04b8937e19abc74f4bfb
Pya Tiluk <pya.tiluk@gmail.com>
2021-12-01 00:14:56 +0100

    save

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ def index():
     flash("What would you like to order?")
     return render_template("index.html")
 
+@app.route("/input", methods = ['POST', 'GET'])
+def input():
+    flash("Hi! Order for" + str(request.form['num_input']) + "is received") 
+    return render_template("index.html")
+
 if __name__ == "__main__":
     app.run()
