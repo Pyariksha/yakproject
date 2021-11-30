fc18e16b237c4ab8dccdfb5b621322d37c0f3944
Pya Tiluk <pya.tiluk@gmail.com>
2021-12-01 00:31:37 +0100

    save

--- app.py
+++ app.py
@@ -2,14 +2,16 @@ from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
 
+app.secret_key = "labyak1"
+
 @app.route("/hello")
 def index():
     flash("What would you like to order?")
     return render_template("index.html")
 
-@app.route("/input", methods = ['POST', 'GET'])
+@app.route("/input", methods = ["POST"])
 def input():
-    flash("Hi! Order for" + str(request.form['num_input']) + "is received") 
+    flash("Hi! Order for " + str(request.form["num_input"]) + " is received") 
     return render_template("index.html")
 
 if __name__ == "__main__":
