5a9856b968c8f3e6fcc504d4c76d0a79bb387312
Pya Tiluk <pya.tiluk@gmail.com>
2021-12-01 12:46:21 +0100

    updates made

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ def index():
     flash("What would you like to order?")
     return render_template("index.html")
 
-@app.route("/input", methods=['GET', 'POST'])
-def input():
-    flash("Hi! Order for " + str(request.form["1_input"]) + " is received") 
-    flash("Hi! Order for " + str(request.form["2_input"]) + " is received") 
+@app.route("/reply", methods=['GET', 'POST'])
+def reply():
+    flash("Hi! Order for " + str(request.form['1_input']) + " litres of milk is received") 
+    flash("Hi! Order for " + str(request.form['2_input']) + " packs of skins is received") 
     return render_template("index.html")
 
 if __name__ == "__main__":
